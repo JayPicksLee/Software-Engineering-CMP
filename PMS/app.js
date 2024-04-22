3992a14879dcbda5d52f320837232dc9121420ba
JayPicksLee <jpicksley@outlook.com>
2024-04-22 21:20:56 +0100

    main page foundation

--- PMS/app.js
+++ PMS/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var login = require('./routes/login');
+var main = require('./routes/main');
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', login);
+app.use('/main', main);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
