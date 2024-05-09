3f299b73f75d93b3085474db3944afa798accf8b
Nich <nich.holt04@gmail.com>
2024-05-09 13:55:01 +0100

    Update register.js

--- PMS/routes/register.js
+++ PMS/routes/register.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 const usermodel = require('../model/users.js');
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.render("register", {title: 'SignUp page'});
 });
 
-/* POST register page. */
+
 router.post('/', (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -16,15 +16,15 @@ router.post('/', (req, res, next) => {
 
   try 
   {
-      // Sign up the user
       console.log("post received");
       usermodel.signUpUser(username, password, email, phoneNumber);
-      // Redirect to login page after successful sign-up
+      
       console.log("Signup successful");
       res.redirect('/login');
       
-  } catch (error) { 
-      // Handle errors, these will be printed to the user
+  } 
+  catch (error) 
+  { 
       res.render('register', { errorMessage: error.message });
   }
 });
