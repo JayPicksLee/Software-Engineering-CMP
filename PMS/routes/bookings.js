372076fca69611e106e9561052b88d96dbc3c54d
JayPicksLee <jpicksley@outlook.com>
2024-05-09 16:53:04 +0100

    Functioning showing of a users requests on bookings page

--- PMS/routes/bookings.js
+++ PMS/routes/bookings.js
@@ -1,8 +1,29 @@
 var express = require('express');
+const fs = require('fs');
+const path = require('path');
 var router = express.Router();
+const requestmodel = require('../model/requests.js');
 
 router.get('/', function(req, res, next) {
-  res.render("bookings", {title: 'Bookings page'});
+
+  const requestDbPath = path.join(__dirname, '..', 'requestdb.json');
+  const requestData = JSON.parse(fs.readFileSync(requestDbPath, 'utf-8'));
+  let data = [];
+
+  let requests = requestmodel.getRequests();
+  console.log(requests);
+  for (const request of requests) {
+    console.log(request);
+    if (request.userID === req.session.userID) {
+
+      data.push(request);
+
+    }else{
+      res.render('bookings', { error: true, message: "Couldn't post requests" });
+    }
+  }
+  console.log("Rendering bookings");
+  res.render("bookings", {data: data});
 });
 
 module.exports = router;
