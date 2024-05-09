7d9ed33161fff70c694f32a6e03693997a1db95a
Nich <nich.holt04@gmail.com>
2024-05-09 14:25:41 +0100

    login account level
    when logging in system now detects your account level (0 or 1) and sends you to the relevant main page depending on your account level

--- PMS/routes/index.js
+++ PMS/routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const fs = require('fs');
+const path = require('path');
 var router = express.Router();
 const usermodel = require('../model/users.js');
 
@@ -16,8 +18,29 @@ router.post('/', (req, res, next)=>
   let loginResult = usermodel.checkLoginDetails(username, password);
   if(loginResult)
   {
-    req.session.userID= usermodel.getUserID(username);
-    res.render("main");
+    const userID = usermodel.getUserID(username);
+    req.session.userID = userID;
+
+    const userDBPath = path.join(__dirname, '..', 'userdb.json');
+    const userData = JSON.parse(fs.readFileSync(userDBPath, 'utf-8'));
+    
+    const user = userData.find(user => user.randomId === userID);
+    
+    if (user) 
+    {
+      if (user.accountLevel === 0) 
+      {
+        res.redirect('/main');
+      } 
+      else if (user.accountLevel === 1) 
+      {
+        res.redirect('/mainAdmin');
+      }
+      else 
+      {
+        res.render('index', { error: true, message: "Invalid account level" });
+      }
+    }
   }
   else
   {
