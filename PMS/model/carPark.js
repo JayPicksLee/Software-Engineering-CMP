0b2b8c708152ddc91d1545e586a48dbb05911512
JayPicksLee <161609517+JayPicksLee@users.noreply.github.com>
2024-05-09 08:58:24 +0000

    Initialized carparks json

---
+++ PMS/model/carPark.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const carParksInfo = JSON.parse(fs.readFileSync('./carParks.json', 'utf-8'));
+
+exports.getCarparks=()=>{
+    return carParksInfo;
+}
+
+exports.getCarparkCapacity=(carparkId)=>{
+    for(let i = 0; i< carParksInfo.length;i++)
+        {
+            if((carParksInfo[i].carparkId === carpkarId))
+            {
+                return carParksInfo[i].capacity;
+            }
+        }
+        return null;
+}
+
