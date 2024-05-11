02304967eecb4bf9e3670930524b6bc25f71a90a
JayPicksLee <jpicksley@outlook.com>
2024-05-11 12:25:05 +0100

    Implemented request creation with Mongodb

--- PMS/model/requests.js
+++ PMS/model/requests.js
@@ -1,14 +1,33 @@
 const fs = require('fs');
 const requestInfo = JSON.parse(fs.readFileSync('./requestdb.json', 'utf-8'));
+const mongoose = require('mongoose');
+
+const Schema=  mongoose.Schema;
+
+const requestSchema = new Schema({
+    userID: String,
+    department: String,
+    destination: String,
+    arriveDate: Date,
+    departDate: Date,
+    approvedStatus: Boolean,
+
+});
+
+const Request = mongoose.model("Request", requestSchema)
 
 exports.getRequests=()=>{
     return requestInfo;
     }
 
-exports.createRequest = (userID,department, destination, arriveDate, departDate) => {
+exports.createRequest = async (userID,department, destination, arriveDate, departDate) => {
     try {
         var approvedStatus = false;
         requestInfo.push({ userID, department, destination, arriveDate, departDate, approvedStatus });
+
+        const newRequest = new Request({userID: userID, department: department, destination: destination, arriveDate: arriveDate, departDate: departDate, approvedStatus: approvedStatus});
+        console.log(newRequest);
+        const savedRequest = await newRequest.save();
 
         fs.writeFileSync('./requestdb.json', JSON.stringify(requestInfo));
     } catch (error) {
