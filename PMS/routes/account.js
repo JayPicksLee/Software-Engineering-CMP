6a0bd251c12a0fabc8abd1d66cd833d5b7421fa6
DeerQuickscoper <npluzek@gmail.com>
2024-05-13 16:53:41 +0100

    added user info to the account page

--- PMS/routes/account.js
+++ PMS/routes/account.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const requestModel = require('../model/requests.js');
 
-router.get(
-  '/', 
-  function(req, res, next) {
-  res.render("account", {title: 'Account page'});
+router.get('/', async (req, res, next) => {
+  try {
+    const userID = req.session.userID; 
+    const userRequests = await requestModel.getRequestsByUserId(userID);
+    res.render('account', { title: 'Account Page', userRequests, userID }); 
+  } catch (error) {
+    console.error('Error fetching requests:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
