a21cb247d1f13defc517fd7c09e8f27049658eeb
DeerQuickscoper <npluzek@gmail.com>
2024-05-14 15:38:02 +0100

    editing css on "ManageAccounts"

--- PMS/routes/account.js
+++ PMS/routes/account.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res, next) => {
   try {
     const userID = req.session.userID; 
     const userRequests = await requestModel.getRequestsByUserId(userID);
-    res.render('account', { title: 'Account Page', userRequests, userID }); 
+    res.render('account', { title: 'Account Page', userRequests, userID}); 
   } catch (error) {
     console.error('Error fetching requests:', error);
     res.status(500).send('Internal Server Error');
